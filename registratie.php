dc4a98b8802336155c3dd5f9092c478efc223b24
Fedor <agargamersinfo@gmail.com>
2024-04-22 03:16:46 +0200

    Onnodige spacing verwijderd

--- registratie.php
+++ registratie.php
@@ -1,8 +1,8 @@
 <?php
-<?php 
-<<<<<<< Updated upstream
 include("database.php");
+?>
 
+<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $voornaam = isset($_POST['Voornaam']) ? $_POST['Voornaam'] : '';
     $tussenvoegsel = isset($_POST['Tussenvoegsel']) ? $_POST['Tussenvoegsel'] : '';
@@ -27,17 +27,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $sqlw = "INSERT INTO KlantReg_Wensen (KlantReg_id, Allergie_id, Voorkeur_id) VALUES ('$LiKlantRegID','$allergieID','$voorkeurID')";
     mysqli_query($conn, $sqlw);
-
-   
-
-    
 }
-=======
-
-
->>>>>>> Stashed changes
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -51,7 +42,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
-<<<<<<< Updated upstream
     <div class="form-container">
         <form action="registratie.php" method="post">
             <h1 id="Gelukt"></h1>
@@ -108,124 +98,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <span class="step"></span>
                 </div>
             </div>
-
-            <!-- <h2>Inkomsten</h2>
-                <label for="Loon">Loon:</label>
-                <input type="number" id="Loon" name="Loon" required>
-                <label for="Uitkering">Uitkering:</label>
-                <input type="number" id="Uitkering" name="Uitkering" required>
-                <label for="KinderBudget">Kinder Budget:</label>
-                <input type="number" id="KinderBudget" name="KinderBudget" required>
-            
-                <?php
-                // $sqlr = "SELECT * FROM Voorkeur";
-                // $resultr = mysqli_query($conn, $sqlr);
-                ?>
-
-                <label for="Voorkeur">Voorkeur:</label>
-                <select name="Voorkeur">
-                    <?php //while($row = mysqli_fetch_assoc($resultr)){
-                    ?>
-                        <option value="<? //= $row['id']; 
-                                        ?>"><? //= $row['Voorkeurnaam']; 
-                                            ?></option>
-                <?php //}
-                ?>
-                </select>
-                <br>
-
-                <?php
-                // $sqlr = "SELECT * FROM Allergie";
-                // $resultr = mysqli_query($conn, $sqlr);
-                ?>
-
-                <label for="Allergie">Allergie:</label>
-                <select name="Allergie">
-                    <?php //while($row = mysqli_fetch_assoc($resultr)){
-                    ?>
-                        <option value="<? //= $row['id']; 
-                                        ?>"><? //= $row['Allergienaam']; 
-                                            ?></option>
-                <?php //}
-                ?>
-                </select>
-                <br>
-                
-                <button type="submit" class="btn">Submit</button> -->
-
         </form>
         <?php
         mysqli_close($conn);
         ?>
     </div>
-            
-=======
-    <h1>Registration Page</h1>
-    <div class="form-container">
-        <form>
-            <h2>Inkomsten</h2>
-            <label for="name1">Loon:</label>
-            <input type="text" id="name1" name="name1" required>
-            <label for="email1">uitkering:</label>
-            <input type="email" id="email1" name="email1" required>
-            <label for="phone1">kinder budget:</label>
-            <input type="text" id="phone1" name="phone1" required>
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-    <div class="form-container">
-        <form>
-            <h2>Uitgaven</h2>
-            <label for="name2">Name:</label>
-            <input type="text" id="name2" name="name2" required>
-            <label for="email2">Email:</label>
-            <input type="email" id="email2" name="email2" required>
-            <label for="phone2">Phone:</label>
-            <input type="text" id="phone2" name="phone2" required>
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-    <div class="form-container">
-        <form>
-            <h2>Gezins samenstelling</h2>
-            <label for="name3">Name:</label>
-            <input type="text" id="name3" name="name3" required>
-            <label for="email3">Email:</label>
-            <input type="email" id="email3" name="email3" required>
-            <label for="phone3">Phone:</label>
-            <input type="text" id="phone3" name="phone3" required>
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-    <div class="form-container">
-        <form>
-            <h2>voorkeuren</h2>
-            <label for="gender">voorkeur:</label>
-            <select id="gender" name="gender">
-                <option value="">Select voorkeur</option>
-                <option value="eten">eten</option>
-                <option value="drinken">drinken</option>
-                <option value="zuivel">Fedor</option>
-            </select>
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-    <div class="form-container">
-        <form>
-            <h2>Allergien</h2>
-            <label for="country">Country:</label>
-            <select id="country" name="country">
-                <option value="">Select Country</option>
-                <option value="usa">USA</option>
-                <option value="canada">Canada</option>
-                <option value="uk">UK</option>
-                <option value="australia">Australia</option>
-            </select>
-            <button type="submit">Submit</button>
-        </form>
-    </div>
->>>>>>> Stashed changes
-
     <script src="registratie.js"></script>
 </body>
 
