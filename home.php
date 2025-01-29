878f51c29ad48245e572196168ca4e62037ef288
Lvanhuijstee <lennyhuijstee@hotmail.com>
2025-01-29 10:20:03 +0100

    frontend changes

--- home.php
+++ home.php
@@ -1,8 +1,8 @@
 <?php
-include('permissions.php');
-if (!isset($_SESSION)) {
-  session_start();
-}
+// include('permissions.php');
+// if (!isset($_SESSION)) {
+//   session_start();
+// }
 ?>
 
 
@@ -16,69 +16,52 @@ if (!isset($_SESSION)) {
   <link rel="stylesheet" media="screen" href="css/Header.css" />
   <link rel="stylesheet" media="screen" href="css/home.css" />
 </head>
-
 <body>
-  <div class="nav">
-    <div class="Name">
-      <div class="logo">
-        <img src="img/mv-vm-letter-logo-vector-29030838.jpg" alt="" />
-      </div>
-      <p>
-        Voedselbank <b />
-        Maaskantje
-      </p>
-    </div>
-    <div class="Menu">
-      <div class="dropMenu">
-        <a id="pakket" href="pakketen.php">
-          Pakket <br />
-          samenstelling
-        </a>
-      </div>
-      <div class="dropMenu">
-        <p>Administratie</p>
-        <div class="dropMenu-Content">
-          <a href="lvs.html">Leverancier overzicht</a>
-          <a href="">Product Overzicht</a>
-          <a href="">Maand overzicht</a>
-          <a href="klanten.html">Klanten overzicht</a>
+<header>
+        <div class="menu-container">
+            <div class="left">
+                <a href="#" class="logo"><img src="img/mv-vm-letter-logo-vector-29030838.jpg" alt=""></a>
+                <h1>Voedselbank Maaskantje</h1>
+            </div>
+            <div class="center">
+                <a href="#">Pakketen samenstelling</a>
+                <div class="dropmenu">
+                    <p >Administratie</p>
+                    <div class="dropmenu-content">
+                    <a href="">eerste</a>
+                    <a href="">tweede</a>
+                    <a href="">derde</a>
+                </div>
+                </div>
+            </div>
+            <div class="right">
+                <a href="#">Account details</a>
+            </div>
         </div>
-      </div>
-      <div class="dropMenu" id="Settings">
-        <p>Account details</p>
-      </div>
-    </div>
-  </div>
-  <div class="flexContainer">
-    <div class="left">
-      <?php
-      $role = Role::from($_SESSION['role']);
-      if ($role->allowed("adminPerms")) {
-      ?><div class="idk">
-          <h2>Leverancier overzicht</h2>
-          <img src="img/truck.png" alt="">
+    </header>
+    <div class="flex-container">
+        <div class="flex-item">
+            <h2>Voorraardbeheer</h2>
+            <div class="voorraad-flex">
+                <div>
+                    <a href="">Voorraad</a>
+                </div>
+            </div>
         </div>
-      <?php
-      } else {
-      }
-      ?>
-      <div class="idk">
-        <h2>Voorraadbeheer</h2>
-        <img src="img/18634-200.png" alt="">
-      </div>
+        <div class="flex-item">
+            <h2>Pakket samenstelling</h2>
+            <div class="pakketen-flex">
+                <div><a href="">Pakketen </a></div>
+                <div><a href="">Producten</a></div>
+            </div>
+        </div>
+        <div class="flex-item">
+            <h2>Gebruiker overzicht</h2>
+            <div class="gebruikers-flex">
+                <div><a href="">Klanten </a></div>
+                <div><a href="">Medewerkers</a></div>
+            </div>
+        </div>
     </div>
-    <div class="centering">
-      <div class="middle">
-        <h2>Pakket samenstelling</h2>
-        <img src="img/list.png" alt="">
-      </div>
-      <div class="right">
-        <h2>Klanten overzicht</h2>
-        <img src="img/2715035-200.png" alt="">
-        <button>Klant gegevens</button>
-      </div>
-    </div>
-  </div>
 </body>
-
 </html>
